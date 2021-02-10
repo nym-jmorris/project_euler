4d7f6351f104e4e921bf32b50252f7c75c3ec0d3
nym-jmorris <jmorris@nymets.com>
2021-02-10 09:24:57 -0500

    p92 in progress

--- problem_092.py
+++ problem_092.py
@@ -18,13 +18,10 @@ def sqrDigits(number):
         sumnum = sumnum + int(n)**2
     return sumnum
 
-limit = 146
+limit = 101
 
 hashtable = {}
 final ={}
-
-eighty9s=[]
-ones = []
 
 for i in range(1,limit):
     nut = i
@@ -32,53 +29,67 @@ for i in range(1,limit):
     while result != 1 and result != 89:
 
         result = sqrDigits(nut)
+        nut = result
 
         if result == 89:
-            eighty9s.append(i)
             hashtable.update({i:89})
             for x, y in hashtable.items():
                 if y == 0:
                     hashtable.update({x:89})
-            
             break
+
         if result == 1:
-            ones.append(i)
             hashtable.update({i:1})
             for x, y in hashtable.items():
                 if y == 0:
                     hashtable.update({x:1})
             break
 
-        # if result in hashtable.keys():
-        #     result = hashtable[result]
-        #     if result == 89:
-        #         eighty9s.append(i)
-        #     if result == 1:
-        #         ones.append(i)
-        #     break
+        # This search is slow. Find a better way to lookup a value
+        # if does not exist, carry on, else, lookup, match and return.
 
-        # hashtable.update({result:0})
+        try:
+            if result in hashtable.keys():
+                if hashtable[result] !=0:
+                    result = hashtable[result]
+                    break
+        except KeyError:
+            hashtable.update({i:0})
 
-        nut = result
+ #       breakpoint()
+
+        hashtable.update({result:0})
     final.update({i:result})
-    # breakpoint()
+    for x, y in hashtable.items():
+        if y == 0:
+            hashtable.update({x:result})
+#    breakpoint()
 
-print(len(eighty9s))
-print(len(ones))
+ones = 0 
+eighty9s =0
+
+for x, y in final.items():
+    if y == 1:
+        ones+=1
+    if y == 89:
+        eighty9s+=1
+
+print(eighty9s)
+print(ones)
 
 f = open('p092_final.txt','w')
 g = open('p092_results.txt','w')
 h = open('p092_hash.txt','w')
 
-for key in final.keys():
+#for key in final.keys():
 #    print(str(key)+': '+str(final[key]))
-    f.write(str(key)+': '+str(final[key])+'\n')
+#    f.write(str(key)+': '+str(final[key])+'\n')
 
 for key in hashtable.keys():
     h.write(str(key)+': '+str(hashtable[key])+'\n')
 
-g.write('There were {} samples run. {} samples resulted in 89 and {} resulted in 1.'.format(limit-1,len(eighty9s),len(ones)))
-g.write('\n{} + {} = {}'.format(len(eighty9s),len(ones),len(eighty9s)+len(ones)))
+g.write('There were {} samples run. {} samples resulted in 89 and {} resulted in 1.'.format(limit-1,eighty9s,ones))
+g.write('\n{} + {} = {}'.format(eighty9s,ones,eighty9s+ones))
 
 f.close
 g.close
