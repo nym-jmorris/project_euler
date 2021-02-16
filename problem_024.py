57b171f358483a85a154f7a5e62bef98969eeaf9
nym-jmorris <jmorris@nymets.com>
2021-02-16 12:41:57 -0500

    p24 complete

--- problem_024.py
+++ problem_024.py
@@ -11,5 +11,50 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 
 '''
 
 digits = [0,1,2,3,4,5,6,7,8,9]
-digits = [0,1,2,3]
-digits = [0,1,2]
+d1 = digits.copy()
+
+perms =[]
+
+for i in d1:
+    d2 = d1.copy()
+    d2.remove(i)
+    
+    for j in d2:
+        d3 = d2.copy()
+        d3.remove(j)
+
+        for k in d3:
+            d4 = d3.copy()
+            d4.remove(k)
+
+            for l in d4:
+                d5 = d4.copy()
+                d5.remove(l)
+
+                for m in d5:
+                    d6 = d5.copy()
+                    d6.remove(m)
+
+                    for n in d6:
+                        d7 = d6.copy()
+                        d7.remove(n)
+
+                        for o in d7:
+                            d8 = d7.copy()
+                            d8.remove(o)
+
+                            for p in d8:
+                                d9 = d8.copy()
+                                d9.remove(p)
+                                
+                                for q in d9:
+                                    d10 = d9.copy()
+                                    d10.remove(q)
+
+                                    for r in d10:
+                                        perm = str(i)+str(j)+str(k)+str(l)+str(m)+str(n)+str(o)+str(p)+str(q)+str(r)
+                                        perms.append(perm)
+
+perms.sort()
+print(perms[999999])
+
