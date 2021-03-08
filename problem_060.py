fe7febf55b3c7066769128eb3854b5d79b310896
nym-jmorris <jmorris@nymets.com>
2021-03-08 16:54:51 -0500

    p60 solved. could be better

--- problem_060.py
+++ problem_060.py
@@ -8,14 +8,18 @@ The sum of these four primes, 792, represents the lowest sum for a set of four p
 
 Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
 '''
-# brute force is a bad idea
-# Works slowly on three digit primes, as these concatenate to six digit primes...
+# brute force works, but can definitely be made better.
 
 # what if we start with large primes and split them into 1/2/3/4 digit smaller primes
 # then we're searching small primes.  Faster?
 # create smaller lists of primes via:
 # j2 = [i for i in j if i <= 5]
 
+
+# 
+# 13, 5197, 5701, 6733 and 8389 meet this condition.
+# Primes sum to 26033
+# Computation took 66.06 seconds
 
 from math import floor
 import time
@@ -67,11 +71,14 @@ def concat(n,m):
 
 time2 = time.time()
 
-for p1 in range(0,len(primes)):
-    
-    for p2 in range(p1+1,len(primes)):
-        
+p1 = 0
+
+
+while p1 < len(primes):
+    p2 = p1+1
+    while p2 < len(primes):
         if getSum(concat(primes[p1],primes[p2])) % 3 == 0:
+            p2 += 1
             continue
         
         if isPrime(concat(primes[p1],primes[p2])) and \
@@ -80,10 +87,16 @@ for p1 in range(0,len(primes)):
             # print('2 Primes found: {} | {}'.format(primes[p1],primes[p2]))
             # print('Time elapsed: {:.2f} seconds\n'.format(time.time()-time2))
 
-            for p3 in range(p2+1,len(primes)):
+            p3 = p2+1
+
+            while p3 < len(primes):
+                
+
                 if getSum(concat(primes[p1],primes[p3])) % 3 == 0:
+                    p3 += 1
                     continue
                 if getSum(concat(primes[p2],primes[p3])) % 3 == 0:
+                    p3 += 1
                     continue
                 if isPrime(concat(primes[p1],primes[p3])) and \
                     isPrime(concat(primes[p3],primes[p1])) and \
@@ -93,12 +106,18 @@ for p1 in range(0,len(primes)):
                             # print('3 Primes found: {} | {} | {}'.format(primes[p1],primes[p2],primes[p3]))
                             # print('Time elapsed: {:.2f} seconds\n'.format(time.time()-time2))
 
-                            for p4 in range(p3+1,len(primes)):
+                            p4 = p3+1
+
+                            while p4 < len(primes):
+
                                 if getSum(concat(primes[p1],primes[p4])) % 3 == 0:
+                                    p4 += 1
                                     continue
                                 if getSum(concat(primes[p2],primes[p4])) % 3 == 0:
+                                    p4 += 1
                                     continue
                                 if getSum(concat(primes[p3],primes[p4])) % 3 == 0:
+                                    p4 += 1
                                     continue
                                 if isPrime(concat(primes[p1],primes[p4])) and \
                                     isPrime(concat(primes[p4],primes[p1])) and \
@@ -107,18 +126,24 @@ for p1 in range(0,len(primes)):
                                                 isPrime(concat(primes[p3],primes[p4])) and \
                                                     isPrime(concat(primes[p4],primes[p3])):
                                                     
-                                                    print('4 Primes found: {} | {} | {} | {}'.format(primes[p1],primes[p2],primes[p3],primes[p4]))
-                                                    print('Time elapsed: {:.2f} seconds\n'.format(time.time()-time2))
+                                                    # print('4 Primes found: {} | {} | {} | {}'.format(primes[p1],primes[p2],primes[p3],primes[p4]))
+                                                    # print('Time elapsed: {:.2f} seconds\n'.format(time.time()-time2))
 
-                                                    for p5 in range(p4+1,len(primes)):
+                                                    p5 = p4 + 1
+
+                                                    while p5 < len(primes):
                                                         
                                                         if getSum(concat(primes[p1],primes[p5])) % 3 == 0:
+                                                            p5 += 1
                                                             continue
                                                         if getSum(concat(primes[p2],primes[p5])) % 3 == 0:
+                                                            p5 += 1
                                                             continue
                                                         if getSum(concat(primes[p3],primes[p5])) % 3 == 0:
+                                                            p5 += 1
                                                             continue
                                                         if getSum(concat(primes[p4],primes[p5])) % 3 == 0:
+                                                            p5 += 1
                                                             continue
                                                         
                                                         if isPrime(concat(primes[p1],primes[p5])) and \
@@ -132,11 +157,14 @@ for p1 in range(0,len(primes)):
                                                                                     print('{}, {}, {}, {} and {} meet this condition.'.format(primes[p1],primes[p2],primes[p3],primes[p4],primes[p5]))
                                                                                     print('Primes sum to {}'.format(primes[p1]+primes[p2]+primes[p3]+primes[p4]+primes[p5]))
                                                                                     print('Computation took {:.2f} seconds'.format(time.time()-time2))
+                                                                                    p5 = len(primes)
+                                                                                    p4 = len(primes)
+                                                                                    p3 = len(primes)
+                                                                                    p2 = len(primes)
+                                                                                    p1 = len(primes)
                                                                                     break
-# this break doesn't exit all the loops
-# The first answer is the right answer. 
-# How to stop once we get there?
-# 
-# 13, 5197, 5701, 6733 and 8389 meet this condition.
-# Primes sum to 26033
-# Computation took 66.06 seconds                                                                                
+                                                        p5 +=1
+                                p4 += 1
+                p3 += 1
+        p2 += 1
+    p1 += 1
